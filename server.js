5ec00593f6097156f19a03e2813f37e798c7b646
mizutombo <thomas.shultz52@gmail.com>
2017-02-26 16:12:11 -0800

    commits base template & folders for back-end

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+const app = require('./lib/app');
+require('mongoose');
+
+require('./lib/connection');
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+	console.log('server is running on ', server.address());
+});
